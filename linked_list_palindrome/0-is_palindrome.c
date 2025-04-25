dd1a11d4bb99f97925e9252cf317c3626d623457
FlorianM-C22 <7484@holbertonstudents.com>
2025-04-25 14:00:42 +0200

    Fix task 0

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -7,11 +7,11 @@
  */
 listint_t *reverse_list(listint_t *head)
 {
-    listint_t *prev = ((void *)0);
+    listint_t *prev = NULL;
     listint_t *current = head;
-    listint_t *next = ((void *)0);
+    listint_t *next = NULL;
 
-    while (current != ((void *)0))
+    while (current != NULL)
     {
         next = current->next;
         current->next = prev;
@@ -34,11 +34,11 @@ int is_palindrome(listint_t **head)
     listint_t *prev_slow = *head;
     int is_pal = 1;
 
-    if (*head == ((void *)0) || (*head)->next == ((void *)0))
+    if (*head == NULL || (*head)->next == NULL)
         return (1);
 
     /* Find the middle of the list */
-    while (fast != ((void *)0) && fast->next != ((void *)0))
+    while (fast != NULL && fast->next != NULL)
     {
         fast = fast->next->next;
         prev_slow = slow;
@@ -46,15 +46,15 @@ int is_palindrome(listint_t **head)
     }
 
     /* Handle odd length */
-    if (fast != ((void *)0))
+    if (fast != NULL)
         slow = slow->next;
 
     /* Reverse the second half */
     second_half = reverse_list(slow);
-    prev_slow->next = ((void *)0);
+    prev_slow->next = NULL;
 
     /* Compare first and second half */
-    while (*head != ((void *)0) && second_half != ((void *)0))
+    while (*head != NULL && second_half != NULL)
     {
         if ((*head)->n != second_half->n)
         {
